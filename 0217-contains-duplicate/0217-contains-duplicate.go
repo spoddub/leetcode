38b0b5152b89d60ce27f828cdf72b3815250e20a
Sergei Poddubny <108150454+spoddub@users.noreply.github.com>
2026-08-05 17:31:30 +0300

    Time: 41 ms (13.25%), Space: 14.2 MB (17.68%) - LeetHub

---
+++ 0217-contains-duplicate/0217-contains-duplicate.go
@@ -0,0 +1,13 @@
+func containsDuplicate(nums []int) bool {
+    seen := make(map[int]bool)
+
+    for _, num := range nums {
+        if _, exists := seen[num]; exists {
+            return true
+        }
+
+        seen[num] = true
+    }
+
+    return false
+}
